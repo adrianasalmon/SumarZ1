26a3170b957994bfdf765d2aab7c2ca2e141967d
adrianasalmon <a2019116341@estudiantes.upsa.edu.bo>
2020-08-29 00:37:04 -0400

    Mi primer codigo

---
+++ Sumar-Restar/Sumar/Controllers/SumaControllercs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sumar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SumaControllercs : ControllerBase
+    {
+        [HttpGet]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
