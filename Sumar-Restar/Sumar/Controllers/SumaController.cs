2725c9c47913d42b8af4caa38ae0548e5d235057
adrianasalmon <a2019116341@estudiantes.upsa.edu.bo>
2020-08-29 01:19:26 -0400

    Mi primer modelo de clase

--- Sumar-Restar/Sumar/Controllers/SumaController.cs
+++ Sumar-Restar/Sumar/Controllers/SumaController.cs
@@ -12,14 +12,28 @@ namespace Sumar.Controllers
     public class SumaController : ControllerBase
     {
         [HttpGet]
+        [Route("Add")]
         public int Add(int a, int b)
         {
             return a + b;
         }
         [HttpPost]
+        [Route("Add")]
         public int Add2([FromHeader] int a, [FromHeader] int b)
         {
             return a + b;
+        }
+        [HttpGet]
+        [Route("Multiply")]
+        public int Add3(int a, int b)
+        {
+            return a * b;
+        }
+        [HttpPost]
+        [Route("Multiply")]
+        public int Add4(int a, int b)
+        {
+            return a * b;
         }
     }
 }
